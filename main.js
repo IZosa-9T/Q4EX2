5aa563e640cf1c7168c2e47057eed35243291e63
Ivy <zosa.ia@obmontessori.edu.ph>
2025-03-25 11:18:42 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+// IF STATEMENT
+
+	// function verify() {
+	// 	let age=document.getElementById('agex').value
+
+	// 	if (age >= 18) {
+	// 		window.alert("Verified! Thank you for your participation.")
+	// 	}
+
+	// }
+
+// IF ELSE STATEMENT
+
+	function verify() {
+		let age=document.getElementById('agex').value
+
+		if (age >= 18) {
+			window.alert("Verified! Thank you for your participation.")
+
+		} else {
+			window.alert("Sorry, you're still underage! Try again in a few years.")
+		}
+
+	}
